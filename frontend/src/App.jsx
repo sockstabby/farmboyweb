0f6e50a466ce0b93ffaed5fc802d8838eabd463b
Your Name <you@example.com>
2023-04-29 22:40:47 -0500

    lots of styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,14 @@
 import { useEffect } from "react";
 import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+import { Tasks } from "./Tasks.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Button from "react-bootstrap/Button";
 
-const style = { display: "flex", gap: "8px", padding: "8px" };
+import reactLogo from "./assets/react.svg";
 
 function App() {
   /**
@@ -17,41 +24,97 @@ function App() {
     }
   }, []);
 
-  return (
-    <BrowserRouter basename="app">
-      <nav style={style}>
-        <Link to="/">Home</Link>
-        <Link to="/settings">Settings Page</Link>
-        <br />
-      </nav>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="settings" element={<SettingsPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
+  return CollapsibleExample();
 
-function SettingsPage() {
+  /*
   return (
-    <div>
-      <h1>Settings Page</h1>
-      <ul>
-        <li>My profile</li>
-        <li>Music</li>
-        <li>About</li>
-      </ul>
+    <div className="container">
+      {" "}
+      <div className="title-bar">
+        <div>TaskMonger</div>
+
+        <img src={reactLogo} className="logo" alt="Vite logo" />
+      </div>
+      <BrowserRouter basename="app">
+        <div className="nav-bar">
+          <nav className="links">
+            <Link to="/">Home</Link>
+            <Link to="/tasks">Tasks</Link>
+            <br />
+          </nav>
+        </div>
+        <div className="main">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="tasks" element={<TasksPage />} />
+          </Routes>
+        </div>
+        <div class="footer"></div>
+      </BrowserRouter>
     </div>
   );
+  */
+}
+
+function TasksPage() {
+  return <Tasks />;
 }
 
 function HomePage() {
-  const style = { padding: "8px" };
+  return <div>Home</div>;
+}
+
+function CollapsibleExample() {
   return (
-    <div style={style}>
-      <h1>React TS Home</h1>
-      <p>Welcome to the homepage</p>
-    </div>
+    <BrowserRouter basename="app">
+      <div>
+        <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+          <Container>
+            <img
+              src={reactLogo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />
+            <Navbar.Brand href="#home">Farm Boy</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto">
+                <Link className="nav-link" to="/">
+                  Tasks
+                </Link>
+                <Link className="nav-link" to="/tasks">
+                  System
+                </Link>
+                <Link className="nav-link" to="/tasks">
+                  Logs
+                </Link>
+                <Link className="nav-link" to="/tasks">
+                  Alerts
+                </Link>
+              </Nav>
+              <Nav>
+                <Nav.Link href="#deets">More deets</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <div className="main">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="tasks" element={<TasksPage />} />
+          </Routes>
+        </div>
+
+        <div className="footer">
+          <div className="footer-text-container">
+            <p>Eric Peterson Copyright @2023</p>
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/Tasks.jsx
@@ -0,0 +1,101 @@
+import { useState, useEffect, useRef, useMemo } from "react";
+import axios from "axios";
+import { AgGridReact } from "ag-grid-react";
+import Button from "react-bootstrap/Button";
+
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
+
+import { FaThumbsUp } from "react-icons/fa";
+import { FaPlus } from "react-icons/fa";
+
+import { FaEdit } from "react-icons/fa";
+
+import { FaSearch } from "react-icons/fa";
+
+export function Tasks() {
+  const [rowData, setRowData] = useState([]);
+
+  const grid = useRef();
+
+  const [columnDefs] = useState([
+    { field: "name" },
+    { field: "taskid" },
+    { field: "schedule" },
+    { field: "enabled" },
+    { field: "config" },
+    { field: "slack" },
+  ]);
+
+  useEffect(() => {
+    axios.get("/api/tasks").then((response) => {
+      setRowData(response.data.data);
+    });
+  }, []);
+
+  const addTask = () => {
+    console.log("add button clicked");
+  };
+
+  const editTask = () => {
+    console.log("add button clicked");
+  };
+
+  const getSelectedRowData = () => {
+    const selectedData = grid.current.api.getSelectedRows();
+    return selectedData;
+  };
+
+  const removeTask = () => {
+    console.log("remove button clicked");
+
+    console.log(getSelectedRowData());
+  };
+
+  const filterGrid = (e) => {
+    const text = e.target.value;
+
+    grid.current.api.setQuickFilter(text);
+  };
+
+  const options = { sortable: true, filter: true };
+  const setDefaultColDef = useMemo(() => options, []);
+
+  return (
+    <>
+      <div className="grid-controls">
+        <div>
+          <FaSearch />
+          <input
+            type="search"
+            onChange={filterGrid}
+            placeholder="Filter Tasks..."
+          ></input>
+        </div>
+        <div>
+          <Button variant="light" onClick={addTask}>
+            <FaPlus />
+          </Button>
+          <Button variant="light" onClick={editTask}>
+            <FaEdit />
+          </Button>
+        </div>
+      </div>
+      <div className="ag-theme-alpine">
+        <AgGridReact
+          ref={grid}
+          defaultColDef={setDefaultColDef}
+          rowData={rowData}
+          rowSelection="single"
+          columnDefs={columnDefs}
+          rowHeight={30}
+        ></AgGridReact>
+      </div>
+      <div className="grid-footer-controls">
+        <Button variant="outline-danger" onClick={removeTask}>
+          Delete Selected Items
+        </Button>{" "}
+      </div>
+    </>
+  );
+}
